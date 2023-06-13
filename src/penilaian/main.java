23533402b652b9fa9cc67406e4dddacd0febe13c
reniohh <renohhreni@gmail.com>
2023-06-13 09:47:03 +0800

    membuat program lathian4

---
+++ src/penilaian/main.java
@@ -0,0 +1,16 @@
+
+package penilaian;
+
+
+public class main {
+     public static void main(String[] args) {
+        Mahasiswa mb=new mahasiswa()
+        
+        System.out.println("Alamat Saya");
+        mb.nama("Reni");
+        mb.npm("kebunserai");
+        mb.uts(04);
+        mb.uas(04);
+        mb.tugas(04);
+     }
+}
